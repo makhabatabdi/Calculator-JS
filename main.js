0655d0623b1435a97baff51dd84df64c319f2c1b
makhabatabd <makhabat9898@gmail.com>
2023-03-21 10:23:46 +0600

    поменяла height calculator на auto и дала display word-wrap: break-word; если будут вводиться очень большие числа

---
+++ main.js
@@ -0,0 +1,33 @@
+let display = document.querySelector(".display");
+
+let buttons = Array.from(document.querySelectorAll(".button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        display.innerText = "0";
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch (e) {
+          display.innerText = "Error!";
+        }
+        break;
+      case "+/-":
+        display.innerText = "-";
+        break;
+      case "%":
+        let passedText = display.innerText + "/100";
+        display.innerText = eval(passedText);
+        break;
+      default:
+        if (display.innerText === "0" && e.target.innerText !== ".") {
+          display.innerText = e.target.innerText;
+        } else {
+          display.innerText += e.target.innerText;
+        }
+    }
+  });
+});
